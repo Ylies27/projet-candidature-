4fdda20b93c4fc6fe84af410fb01e58f58dc5aea
ylies tamrani <ylies.tamrani@gmail.com>
2021-07-27 16:44:42 +0200

    commit intial du projet python

---
+++ main.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+essaie = 3
+i = 0
+annee = ['1994','1998','2002','2006','2010','2014','2018']
+pays = ['bresil', 'france', 'bresil', 'italie', 'espagne', 'allemagne', 'france']
+
+while essaie > 0 and i < len(annee):
+    print("Essaie " + str(essaie) +"/3")
+    print("Question " + str(i + 1) +"/7: Quelle equipe a gagne la coupe du monde de "+ annee[i]+ "?")
+    reponse = input()
+
+    while reponse == None or len(reponse) == 0:
+        print("Veuillez entrer une reponse valide.")
+        reponse = input() 
+
+    if(reponse.lower() != pays[i]):
+        essaie -= 1
+        print("Reponse incorrect. Vous avez " + str(essaie) + " essaie restant. \n\n")
+    else:    
+        print("Correct! \n\n")
+        i += 1
+
+if(essaie == 0):
+    print("Nous n'avez pas reussi a finir le questionnaire. Reesayer une autre fois.")
+else:
+    print("Bravo!! Vous avez reussi a finir le quiz avec "+ str(3-essaie) + " erreur(s).")
